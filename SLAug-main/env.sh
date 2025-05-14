08b661dff286b51f3ea23de99b3b27f9554e5047
Hao Chen <ch981129@126.com>
2025-05-15 00:25:35 +0100

    prostate test

---
+++ SLAug-main/env.sh
@@ -0,0 +1,6 @@
+
+conda create -n slaug python=3.10
+
+
+conda  activate slaug
+pip install -r requirements.txt   -f https://download.pytorch.org/whl/torch_stable.html
---
+++ SLAug-main/run.sh
@@ -0,0 +1,7 @@
+
+
+
+python main.py --base configs/efficientUnet_Prostate.yaml --seed 23
+
+
+
